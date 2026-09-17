961f2167cf1b9bc7ca96efb2adf849af005a9362
eubob <brunnoo365@gmail.com>
2026-09-16 21:40:14 -0300

    Commit inicial do projeto tkweb

---
+++ src/main/java/br.com/mvc/config/MysqlSingleton.java
@@ -0,0 +1,35 @@
+package br.com.mvc.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MysqlSingleton {
+    private static final String URL = "jdbc:mysql://localhost:3306/mvcjava?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = ""; 
+    private static MysqlSingleton instance;
+    private Connection conexao;
+
+    private MysqlSingleton() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver MySQL nao encontrado no projeto.", e);
+        }
+    }
+
+    public static synchronized MysqlSingleton getInstance() {
+        if (instance == null) {
+            instance = new MysqlSingleton();
+        }
+        return instance;
+    }
+
+    public Connection obterConexao() throws SQLException {
+        if (this.conexao == null || this.conexao.isClosed()) {
+            this.conexao = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return this.conexao;
+    }
+}
